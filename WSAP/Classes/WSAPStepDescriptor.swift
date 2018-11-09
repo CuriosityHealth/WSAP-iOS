6e520f876bdd482e3abcabbb48aabb52c618987b
James Kizer <james.kizer@gmail.com>
2018-11-09 09:59:28 -0700

    Moved standard step descriptor

--- WSAP/Classes/WSAPStepDescriptor.swift
+++ WSAP/Classes/WSAPStepDescriptor.swift
@@ -20,36 +20,3 @@ open class WSAPStepDescriptor: RSTBStepDescriptor {
     }
     
 }
-open class WSAPStandardStepDescriptor: WSAPStepDescriptor {
-    
-    public let crossTime: TimeInterval
-    public let wordTime: TimeInterval
-    public let relatedIdentifierSuffix: String
-    public let unrelatedIdentifierSuffix: String
-    public let affirmativeButtonText: String
-    public let negativeButtonText: String
-    public let numberOfSentences: Int?
-    
-    
-    public required init?(json: JSON) {
-        
-        guard let crossTime: TimeInterval = "cross_time" <~~ json,
-            let wordTime: TimeInterval = "word_time" <~~ json,
-            let relatedIdentifierSuffix: String = "related_identifier_suffix" <~~ json,
-            let unrelatedIdentifierSuffix: String = "unrelated_identifier_suffix" <~~ json,
-            let affirmativeButtonText: String = "affirmative_button_text" <~~ json,
-            let negativeButtonText: String = "negative_button_text" <~~ json else {
-                return nil
-        }
-        
-        self.crossTime = crossTime
-        self.wordTime = wordTime
-        self.relatedIdentifierSuffix = relatedIdentifierSuffix
-        self.unrelatedIdentifierSuffix = unrelatedIdentifierSuffix
-        self.affirmativeButtonText = affirmativeButtonText
-        self.negativeButtonText = negativeButtonText
-        self.numberOfSentences = "numberOfSentences" <~~ json
-        super.init(json: json)
-    }
-
-}
